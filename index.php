c2eb59a9663c4d22726633edfd99df07b7c6c576
Liquer-Cami <camilaliquer@gmail.com>
2025-07-04 18:27:20 -0300

    Add navigation bar and page routing for user management

--- index.php
+++ index.php
@@ -10,7 +10,46 @@
     <title>Cadastro</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Cadastro</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=new">Novo Usuário</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="?page=list">Listar Usuários</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col mt-5">
+            <?php
+                switch(@$_REQUEST["page"]){
+                    case "new":
+                        include("new-user.php");
+                    break;
+                    case "list":
+                        include("list-users.php");
+                    break;
+                    default:
+                        print "<h1>Bem vindos!</h1>";
+                }
+            ?>
+        </div>
+    </div>
+</div>
 
     <script src="js/bootstrap.bundle.min.js"></script>
     
