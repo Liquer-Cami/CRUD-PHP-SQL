34cbfdde4ecd088fdd94760fae2ea2a809451a18
Liquer-Cami <camilaliquer@gmail.com>
2025-07-10 16:10:37 -0300

    Implement edit user functionality: update user details and enhance form handling

--- edit-user.php
+++ edit-user.php
@@ -1,24 +1,30 @@
 <h1>Editar Usuário</h1>
+<?php
+    $sql = "SELECT * FROM usuarios WHERE id=".$_REQUEST["id"];
+    $res = $conn->query($sql);
+    $row = $res->fetch_object();
+?>
 <form action="?page=save" method="POST">
-    <input type="hidden" name="action" value="create">
+    <input type="hidden" name="action" value="edit">
+    <input type="hidden" name="id" value="<?php print $row->id; ?>">
     <div class="mb-3">
         <label>Nome</label>
-        <input type="text" name="nome"
+        <input type="text" name="nome" value="<?php print $row->nome; ?>"
         class="form-control">
     </div>
     <div class="mb-3">
         <label>Email</label>
-        <input type="email" name="email" 
+        <input type="email" name="email" value="<?php print $row->email; ?>"
         class="form-control">
     </div>
     <div class="mb-3">
         <label>Senha</label>
-        <input type="password" name="senha" 
-        class="form-control">
+        <input type="password" name="senha"
+        class="form-control" required>
     </div>
     <div class="mb-3">
         <label>Data de Nascimento</label>
-        <input type="date" name="data_nasc" 
+        <input type="date" name="data_nasc" value="<?php print $row->data_nasc; ?>"
         class="form-control">
     </div>
     <div class="mb-3">
--- save-user.php
+++ save-user.php
@@ -21,7 +21,30 @@
                 print"<script>location.href='?page=new';</script>"; 
             }
             break;
-        case 'update':
+        case 'edit':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $data_nasc = $_POST["data_nasc"];
+
+            $sql = "UPDATE usuarios SET
+                        nome='{$nome}',
+                        email='{$email}',
+                        senha='{$senha}',
+                        data_nasc='{$data_nasc}'
+                    WHERE
+                        id=".$_REQUEST["id"];
+
+            $res = $conn->query($sql);
+
+            if($res==true){
+                print"<script>alert('Editado com sucesso!');</script>";
+                print"<script>location.href='?page=list;</script>";
+            }
+            else{
+                print"<script>alert('Erro ao editar!');</script>";
+                print"<script>location.href='?page=new';</script>"; 
+            }
             break;
         case 'delete':
             break;
